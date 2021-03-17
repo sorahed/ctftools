3b2fa67d509c9bf4201ccd86bbad7bfb1fd1a8b1
sofxwy <68956174+sofxwy@users.noreply.github.com>
2021-03-17 15:26:42 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    from packages.ctf import menu, linpeas, winpeas, openvpn, serverpython
+    menu()
+    linpeas()
+    winpeas()
+    openvpn()
+    serverpython()
+     
